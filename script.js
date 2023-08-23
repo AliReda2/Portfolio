bc9ca24f1f1f326c63a341f0ab40a895f6514546
AliReda2 <alireda2572003@gmail.com>
2023-08-23 19:23:59 +0300

    mouse

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const cursorSmall = document.querySelector('.small');
 const cursorBig = document.querySelector('.big');
-
 const positionElement = (e) => {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
 
-    cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY+totalScrollY}px, 0)`;
+    cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY + totalScrollY}px, 0)`;
 
-    cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY+totalScrollY}px, 0)`;
+    cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY + totalScrollY}px, 0)`;
 
 }
-
 window.addEventListener('mousemove', positionElement)
-
-
 let clientScrollY = 0;
 let totalScrollY = 0;
 
 function updateTotalScrollY() {
     totalScrollY = window.scrollY + clientScrollY;
-    console.log(totalScrollY);
 }
 
 // document.addEventListener('mousemove', (e1) => {
@@ -29,3 +24,19 @@ function updateTotalScrollY() {
 document.addEventListener('scroll', (e) => {
     updateTotalScrollY();
 })
+
+const link=document.querySelector('a');
+
+// link.addEventListener("mouseover",getBig());
+function getBig() {
+    cursorBig.style.width="40px";
+    cursorBig.style.height="40px";
+    cursorBig.style.top="-20px";
+    cursorBig.style.left="-20px";
+}
+function getSmall() {
+    cursorBig.style.width="20px";
+    cursorBig.style.height="20px";
+    cursorBig.style.top="-11px";
+    cursorBig.style.left="-11px";
+}
