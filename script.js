9f2b8ca376ae8772943ca720e48620739df72b42
AliReda2 <alireda2572003@gmail.com>
2023-08-24 17:20:51 +0300

    test

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cursorSmall = document.querySelector('.small');
 const cursorBig = document.querySelector('.big');
+// loadEventListeners();
 const positionElement = (e) => {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
@@ -25,18 +26,32 @@ document.addEventListener('scroll', (e) => {
     updateTotalScrollY();
 })
 
-const link=document.querySelector('a');
 
-// link.addEventListener("mouseover",getBig());
+var elements = document.getElementsByTagName('a');
+for(var i = 0, len = elements.length; i < len; i++) {
+    elements[i].onmouseover = getBig;
+}
+for(var i = 0, len = elements.length; i < len; i++) {
+    elements[i].onmouseout = getSmall;
+}
+
 function getBig() {
-    cursorBig.style.width="40px";
-    cursorBig.style.height="40px";
-    cursorBig.style.top="-20px";
-    cursorBig.style.left="-20px";
+    cursorBig.style.width = "40px";
+    cursorBig.style.height = "40px";
+    cursorBig.style.top = "-20px";
+    cursorBig.style.left = "-20px";
 }
 function getSmall() {
-    cursorBig.style.width="20px";
-    cursorBig.style.height="20px";
-    cursorBig.style.top="-11px";
-    cursorBig.style.left="-11px";
+    cursorBig.style.width = "20px";
+    cursorBig.style.height = "20px";
+    cursorBig.style.top = "-11px";
+    cursorBig.style.left = "-11px";
+}
+const spanx=document.getElementById('jobs');
+setInterval(changeJob,2000);
+var i=0;
+function changeJob() {
+    const jobs=['Websites','MobileApps','Things','WebApps'];
+    spanx.innerText= jobs[i];
+    i=(i+1)%jobs.length;
 }
